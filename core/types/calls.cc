c9ab6ac58a9f595c802294c60fa3d954cade01d3
Trevor Elliott <awesomelyawesome@gmail.com>
2019-10-31 16:35:22 -0700

    Disable suggestions for missing methods

--- core/types/calls.cc
+++ core/types/calls.cc
@@ -525,6 +525,7 @@ DispatchResult dispatchCallSymbol(Context ctx, DispatchArgs args,
                                                              objMeth.data(ctx)->owner.data(ctx)->name.show(ctx))));
                     }
                 }
+                /*
                 auto alternatives = symbol.data(ctx)->findMemberFuzzyMatch(ctx, args.name);
                 if (!alternatives.empty()) {
                     vector<ErrorLine> lines;
@@ -576,6 +577,7 @@ DispatchResult dispatchCallSymbol(Context ctx, DispatchArgs args,
                     }
                     e.addErrorSection(ErrorSection(lines));
                 }
+                */
 
                 auto attached = symbol.data(ctx)->attachedClass(ctx);
                 if (attached.exists() && symbol.data(ctx)->derivesFrom(ctx, Symbols::Chalk_Tools_Accessible())) {
